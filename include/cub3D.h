89eb664db5d5d2e5bb2e9620dbbeb5bdc3f68604
ViniciusSantoro <vinicius.rio@ifsp.edu.br>
2022-11-21 20:38:10 -0300

    feat: :rocket: add libft

---
+++ include/cub3D.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vnazioze <vnazioze@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 20:33:41 by vnazioze          #+#    #+#             */
+/*   Updated: 2022/11/21 20:33:41 by vnazioze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <mlx.h>
+# include <stdlib.h>
+# include <math.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
+
+#endif
