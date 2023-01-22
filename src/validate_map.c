aaadb38845da6f95e3e27b905d5aec4eb1ef8040
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2023-01-22 18:48:54 -0300

    refact: is_valid_ext

--- src/validate_map.c
+++ src/validate_map.c
@@ -6,17 +6,32 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 17:01:31 by ldatilio          #+#    #+#             */
-/*   Updated: 2023/01/22 16:09:22 by ldatilio         ###   ########.fr       */
+/*   Updated: 2023/01/22 18:36:12 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
 
+int is_valid_ext(const char *file, const char *ext)
+{
+    size_t	file_len;
+	size_t	ext_len;
+	
+    file_len = ft_strlen(file);
+    ext_len = ft_strlen(ext);
+    if (file_len < ext_len)
+        return (0);
+    if (ft_strncmp(file + (file_len - ext_len), ext, ext_len) == 0)
+        return (1);
+    return (0);
+}
+
 char	*get_tex_file(char *line, char *tex, t_data *data)
 {
 	char set[] = " \t\n\r";
     char *file_name = ft_strtrim(line + 3, set);
-    printf("\nNome do arquivo: %s\n", file_name);
+	if (is_valid_ext(file_name, ".xpm") == FALSE)
+		error_message(1, "The file extension is not '.xpm'");
     return (file_name);
 }
 
@@ -31,17 +46,6 @@ void	check_line(char *line, t_data *data)
 		data->tex.we = get_tex_file(line, data->tex.we, data);
 	else if (ft_strncmp("EA ", line, 3) == 0)
 		data->tex.ea = get_tex_file(line, data->tex.ea, data);
-}
-
-int	is_ext_cub(const char *file)
-{
-	size_t	len;
-
-	len = ft_strlen(file);
-	if (file[len - 4] != '.' || file[len - 3] != 'c' || \
-		file[len - 2] != 'u' || file[len - 1] != 'b')
-		return (0);
-	return (1);
 }
 
 int	read_map(int fd, t_data *data)
@@ -72,10 +76,8 @@ int	validate_map(const char *file_name, t_data *data)
 	fd = open(file_name, O_RDONLY);
 	if (fd < 0)
 		error_message(1, "File is not exist");
-	if (is_ext_cub(file_name) == FALSE)
+	if (is_valid_ext(file_name, ".cub") == FALSE)
 		error_message(1, "The file extension is not '.cub'");
-	else
-		printf("\nThe file extension is '.cub'");
 	read_map(fd, data);
 	close(fd);
 	return (0);
