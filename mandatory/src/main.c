ae6a5ef563abd6230b89979eefcd6a79cd151141
Vinicius Santoro <viniciussantoro@outlook.com>
2023-02-25 21:37:42 +0100

    fix: :hammer: RONLY to RDWR

--- mandatory/src/main.c
+++ mandatory/src/main.c
@@ -42,7 +42,7 @@ static int	validate_map(char *file_name, t_data *data)
 {
 	int	fd;
 
-	fd = open(file_name, O_RDONLY);
+	fd = open(file_name, O_RDWR);
 	if (fd < 0)
 		error_message(3, "Invalid file: not exist", data);
 	if (is_valid_ext(file_name, ".cub") == FALSE)
